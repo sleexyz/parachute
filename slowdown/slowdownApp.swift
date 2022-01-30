4302c085465df3e64e34194851edb1517aa75041
Sean Lee <freshdried@gmail.com>
2022-01-30 11:16:11 -0500

    Initial Commit

---
+++ slowdown/slowdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  slowdownApp.swift
+//  slowdown
+//
+//  Created by Sean Lee on 1/30/22.
+//
+
+import SwiftUI
+
+@main
+struct slowdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
