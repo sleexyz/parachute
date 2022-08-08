ed746ad04677abf6365b5273fedd6aaab057d597
Sean Lee <freshdried@gmail.com>
2022-08-08 02:33:54 -0400

    Add ip route debugging information.

--- go-proxy/main.go
+++ go-proxy/main.go
@@ -5,15 +5,20 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/netip"
+
 	"os"
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
+	"golang.zx2c4.com/go118/netip"
+	"golang.zx2c4.com/wireguard/tun"
+	"golang.zx2c4.com/wireguard/tun/netstack"
 )
 
 type Server struct {
 	conn *net.UDPConn
+	tun  tun.Device
+	tnet *netstack.Net
 }
 
 func (c *Server) Listen() {
@@ -31,6 +36,16 @@ func (c *Server) Listen() {
 			} else {
 				packet = gopacket.NewPacket(data[:n], layers.LayerTypeIPv4, gopacket.Default)
 			}
+			if ipv4Layer := packet.Layer(layers.LayerTypeIPv4); ipv4Layer != nil {
+				fmt.Println("This is a ipv4 packet!")
+				ipv4, _ := ipv4Layer.(*layers.IPv4)
+				fmt.Printf("From src address %d to dst address %d\n", ipv4.SrcIP, ipv4.DstIP)
+			}
+			if ipv6Layer := packet.Layer(layers.LayerTypeIPv6); ipv6Layer != nil {
+				fmt.Println("This is a ipv6 packet!")
+				ipv6, _ := ipv6Layer.(*layers.IPv6)
+				fmt.Printf("From src address %d to dst address %d\n", ipv6.SrcIP, ipv6.DstIP)
+			}
 			if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
 				fmt.Println("This is a TCP packet!")
 				// Get actual TCP data from this layer
@@ -41,7 +56,10 @@ func (c *Server) Listen() {
 			for _, layer := range packet.Layers() {
 				fmt.Println("PACKET LAYER:", layer.LayerType())
 			}
-			// conn.WriteToUDP(data[:n], remoteAddr)
+			_, err := c.tun.Write(data[:n], 0)
+			if err != nil {
+				fmt.Printf("bad write: %s", err)
+			}
 		}
 	}
 }
@@ -52,18 +70,16 @@ func main() {
 		port = "8080"
 	}
 
-	// tun, tnet, err := netstack.CreateNetTUN(
-	// 	[]netip.Addr{netip.MustParseAddr("0.0.0.0")},
-	// 	[]netip.Addr{netip.MustParseAddr("8.8.8.8")},
-	// 	8080)
-	// if err != nil {
-	// 	log.Panicln(err)
-	// }
-	// tun.write
-	// listener, err := tnet.ListenUDP(&net.UDPAddr{Port: port})
-	// if err != nil {
-	// 	log.Panicln(err)
-	// }
+	tun, tnet, err := netstack.CreateNetTUN(
+		[]netip.Addr{netip.MustParseAddr("10.0.0.8")},
+		[]netip.Addr{netip.MustParseAddr("8.8.8.8")}, // DNS
+		1500)
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	// TODO: configure TUN to write packets forward
+	// TODO: listen on TUN to write packets back
 
 	conn, err := net.ListenUDP("udp", &net.UDPAddr{IP: netip.MustParseAddr("0.0.0.0").AsSlice(), Port: 8080})
 	if err != nil {
@@ -72,6 +88,6 @@ func main() {
 	defer conn.Close()
 
 	log.Printf("Listening on port %s", port)
-	server := Server{conn: conn}
+	server := Server{conn: conn, tun: tun, tnet: tnet}
 	server.Listen()
 }
