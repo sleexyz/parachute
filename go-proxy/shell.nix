aa2105b1d9a59c2d10dee2d5bb6013dfdc0a6485
Sean Lee <freshdried@gmail.com>
2022-10-27 18:49:07 -0400

    Modify shell.nix to use the current system xcode installation.

--- go-proxy/shell.nix
+++ go-proxy/shell.nix
@@ -1,6 +1,15 @@
 { pkgs ? import <nixpkgs> {}}:
-
-pkgs.mkShell {
+let
+  xcodewrapper = pkgs.xcodeenv.composeXcodeWrapper {
+    version = "14.0.1";
+    xcodeBaseDir = "/Applications/Xcode.app";
+  };
+in
+pkgs.stdenv.mkDerivation {
+  name = "go-proxy-shell";
+  shellHook = ''
+    export PATH=${xcodewrapper}/bin:$PATH
+  '';
   nativeBuildInputs = with pkgs; [ 
     go_1_19
     entr
