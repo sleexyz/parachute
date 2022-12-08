b1d754c7f90f0a601ae51bbf1a0549fbb6b30e7e
Sean Lee <freshdried@gmail.com>
2022-12-07 21:10:22 -0500

    Move cheat logic to CheatController.

--- slowdown/VPNConfigurationService.swift
+++ slowdown/VPNConfigurationService.swift
@@ -8,7 +8,10 @@
 import NetworkExtension
 import Foundation
 import os
-import UserNotifications
+
+enum UserError: Error {
+    case message(message: String)
+}
 
 final class VPNConfigurationService: ObservableObject {
     @Published private(set) var isInitializing = true
@@ -16,20 +19,8 @@ final class VPNConfigurationService: ObservableObject {
     @Published var isTransitioning = false
     @Published private var manager: NETunnelProviderManager?
     
-    @Published private var cheatDeadline: Date?
-    
     var hasManager: Bool {
         return manager != nil
-    }
-    
-    var isCheating: Bool {
-        return cheatDeadline != nil && cheatDeadline! > Date()
-    }
-    var cheatTimeLeft: Int {
-        if cheatDeadline == nil {
-            return 0
-        }
-        return Int(cheatDeadline!.timeIntervalSinceNow)
     }
     
     let store: SettingsStore
@@ -62,88 +53,30 @@ final class VPNConfigurationService: ObservableObject {
                 self.isTransitioning = true
             }
         }
-        
-        // Register to receive notification in your class
-        NotificationCenter.default.addObserver(forName: .NEVPNConfigurationChange, object: nil, queue: nil) { notification in
-            os_log("notification: \(notification.description)")
-            //            let conn = notification.object as! NEVPNConnection
-            //            if conn.status == NEVPNStatus.connected {
-            //                self.connected = true
-            //                self.isLoading = false;
-            //
-            //            }
-            //            if conn.status == NEVPNStatus.connecting{
-            //                self.isLoading = true
-            //            }
-            //            if conn.status == NEVPNStatus.disconnected {
-            //                self.connected = false
-            //                self.isLoading = false;
-            //            }
-            //            if conn.status == NEVPNStatus.disconnecting{
-            //                self.isLoading = true
-            //            }
-        }
     }
     
-    func enableNotifications() async throws -> Bool {
-        let center = UNUserNotificationCenter.current()
-        let enabled = try await withCheckedThrowingContinuation { continuation in
-            center.getNotificationSettings { settings in
-                continuation.resume(with: .success(settings.alertSetting == .enabled))
-            }
-        }
-        if enabled {
-            return true
-        }
-        let granted: Bool = try await withCheckedThrowingContinuation { continuation in
-            center.requestAuthorization(options: [.alert]) { granted, error in
-                if let error = error {
-                    continuation.resume(with: .failure(error))
-                    return
-                } else {
-                    continuation.resume(with: .success(granted))
-                }
-            }
-        }
-        return granted
-    }
-    
-    func startCheat() async throws {
-        let _ = try await enableNotifications()
-        
+    func startCheat() async throws -> () {
+        let message = "cheat".data(using: String.Encoding.utf8)
         guard let session = self.manager?.connection as? NETunnelProviderSession else {
             return
         }
-        
-        let message = "cheat".data(using: String.Encoding.utf8)
-        try session.sendProviderMessage(message!) { response in
-            if response != nil {
-                let responseString = NSString(data: response!, encoding: String.Encoding.utf8.rawValue)
-                os_log("Received response from the provider: \(responseString)")
-            } else {
-                os_log("Got a nil response from the provider")
+        return try await withCheckedThrowingContinuation { continuation in
+            do {
+                try session.sendProviderMessage(message!) { response in
+                    if response != nil {
+                        let responseString = NSString(data: response!, encoding: String.Encoding.utf8.rawValue)
+                        os_log("Received response frommm the provider: \(responseString)")
+                        continuation.resume(returning: ())
+                    } else {
+                        os_log("Got a nil response from the provider")
+                        continuation.resume(throwing: UserError.message(message: "Got a nil response from the provider"))
+                    }
+                }
+            } catch let error {
+                continuation.resume(with: .failure(error))
+                
             }
         }
-        DispatchQueue.main.async {
-            self.cheatDeadline = Date().addingTimeInterval(60)
-        }
-        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(60)) {
-                    os_log("invalidating cheat time")
-            self.cheatDeadline = nil
-        }
-        try await self.sendMessage(title: "Cheat ended", body: "", fromNow: 60)
-    }
-    
-    func sendMessage(title: String, body: String, fromNow: TimeInterval) async throws {
-        let content = UNMutableNotificationContent()
-        content.title = title
-        content.body = body
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: fromNow, repeats: false)
-        let uuidString = UUID().uuidString
-        let request = UNNotificationRequest(identifier: uuidString,
-                                            content: content, trigger: trigger)
-        let notificationCenter = UNUserNotificationCenter.current()
-        return try await notificationCenter.add(request)
     }
     
     
@@ -162,19 +95,6 @@ final class VPNConfigurationService: ObservableObject {
         self.isTransitioning = true
     }
     
-    private func saveToPreferences() {
-        //        isLoading = true
-        self.manager!.saveToPreferences { [weak self] error in
-            //            guard let self = self else { return }
-            //            self.isLoading = false
-            
-            //            if let error = error {
-            //                self.showError(title: "Failed to update VPN configuration", message: error.localizedDescription)
-            //                self.errorMessage = error.localizedDescription
-            //                return
-            //            }
-        }
-    }
     
     func installProfile(_ completion: @escaping (Result<Void, Error>) -> Void) {
         let tunnel = makeTunnel()
