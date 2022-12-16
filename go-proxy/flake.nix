b97f83c3071fc6a2e3db698acc505e303fdf74aa
Sean Lee <freshdried@gmail.com>
2022-12-16 13:11:38 -0500

    Switch to flakes.

---
+++ go-proxy/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "slowdown-dev-shell";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/22.11";
+  outputs =
+    { self , nixpkgs , }:
+    let
+      pkgs = import nixpkgs { system = "aarch64-darwin"; };
+      xcodewrapper = pkgs.xcodeenv.composeXcodeWrapper {
+        version = "14.1";
+        xcodeBaseDir = "/Applications/Xcode.app";
+      };
+    in
+    {
+      devShells.aarch64-darwin.default = pkgs.mkShell {
+        name = "slowdown-dev-shell";
+        nativeBuildInputs = with pkgs; [
+          go_1_19
+          entr
+          graphviz # for pprof
+          llvmPackages_11.clang
+          buildifier
+          bazel_5
+        ];
+        shellHook = ''
+          export PATH=${xcodewrapper}/bin:$PATH
+          
+          export GOPATH=$(pwd)/.gopath
+          export PATH=$PATH:$GOPATH/bin
+          export CGO_ENABLED=1
+        '';
+      };
+    };
+}
+
