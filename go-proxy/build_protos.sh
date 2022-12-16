46717bdc6dccf83494e1acf8953499e6d91ee2fa
Sean Lee <freshdried@gmail.com>
2022-12-16 18:59:52 -0500

    Get protos working with any.

--- go-proxy/build_protos.sh
+++ go-proxy/build_protos.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# if protoc -I=protos --go_out=. protos/proxyservice.proto; then
 if protoc -I=protos --go_out=. --swift_out=ProxyService/Sources/ProxyService --swift_opt=Visibility=Public protos/proxyservice.proto; then
   echo "successfully generated protos"
 fi
