4159a81d501b4389a919c6890fc087c2f2401edf
Sean Lee <freshdried@gmail.com>
2023-01-08 20:04:16 -0500

    Add crashlytics to main app.

--- slowdown/slowdownApp.swift
+++ slowdown/slowdownApp.swift
@@ -9,15 +9,29 @@ import SwiftUI
 import ProxyService
 import Logging
 import LoggingOSLog
+import Firebase
 
 @main
 struct slowdownApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     init() {
         LoggingSystem.bootstrap(LoggingOSLog.init)
     }
     var body: some Scene {
         WindowGroup {
+            Button("Crash") {
+              fatalError("Crash was triggered")
+            }
             ContentView()
         }
     }
 }
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
