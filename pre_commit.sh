e1c566722ea8653c4053173ab557cce547b928de
Sean Lee <freshdried@gmail.com>
2023-02-16 20:03:21 -0500

    Include untracked changes in precommit hook abort check.

--- pre_commit.sh
+++ pre_commit.sh
@@ -5,8 +5,17 @@ set -e
 cd "$(git rev-parse --show-toplevel)"
 eval "$(direnv export bash)"
 
-if [[ $(git diff --stat) != '' ]]; then
-    echo "Working tree is dirty, aborting commit."
+
+if [[ "$(git diff --stat)" != '' ]]; then
+    echo "Working tree is dirty, aborting commit:"
+    git status --short
+    exit 1
+fi
+
+UNTRACKED_FILES="$(git ls-files . --exclude-standard --others)"
+if [[ "${UNTRACKED_FILES}" != '' ]]; then
+    echo "Found untracked files, aborting commit:"
+    echo "${UNTRACKED_FILES}"
     exit 1
 fi
 
