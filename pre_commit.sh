00cee9b7d0471dd92af264b5c4d441b030e8096c
Sean Lee <freshdried@gmail.com>
2023-02-16 19:52:30 -0500

    Exit precommit if working tree is dirty.

--- pre_commit.sh
+++ pre_commit.sh
@@ -5,5 +5,10 @@ set -e
 cd "$(git rev-parse --show-toplevel)"
 eval "$(direnv export bash)"
 
+if [[ $(git diff --stat) != '' ]]; then
+    echo "Working tree is dirty, aborting commit."
+    exit 1
+fi
+
 make clean
 make
