0c7f8cc3f011ca632e211d1b5ad15825827baee2
Sean Lee <freshdried@gmail.com>
2023-08-31 15:08:53 -0400

    Try again (no-op).

--- ParachuteApp/ContentView.swift
+++ ParachuteApp/ContentView.swift
@@ -95,8 +95,6 @@ struct ContentViewFamilyControls_Previews: PreviewProvider {
     }
 }
 
-
-
 struct ContentViewContentFilter_Previews: PreviewProvider {
     static var previews: some View {
         ContentView(testOnlyAuthorizationStatusOverride: .approved)
