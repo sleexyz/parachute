d2c2727043236fd17f824f0457fdaa348d45ac6b
Sean Lee <freshdried@gmail.com>
2023-09-01 13:18:44 -0400

    One signal init wip.

--- ParachuteApp/slowdownApp.swift
+++ ParachuteApp/slowdownApp.swift
@@ -14,6 +14,9 @@ import FirebaseCore
 import OSLog
 import ProxyService
 import SwiftUI
+import OneSignalFramework
+
+let ONESIGNAL_APP_ID = "b1bee63a-1006-42bd-bd66-5a803c64f63c"
 
 @main
 struct slowdownApp: App {
@@ -48,6 +51,23 @@ class AppDelegate: NSObject, UIApplicationDelegate {
             }
         }
         NEConfigurationService.shared.registerBackgroundTasks()
+        // NEConfigurationService.shared.registerBackgroundTasks()
+
+        // Remove this method to stop OneSignal Debugging
+        OneSignal.Debug.setLogLevel(.LL_VERBOSE)
+            
+        // OneSignal initialization
+        OneSignal.initialize(ONESIGNAL_APP_ID, withLaunchOptions: launchOptions)
+
+        // requestPermission will show the native iOS notification permission prompt.
+        // We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+        OneSignal.Notifications.requestPermission({ accepted in
+            print("User accepted notifications: \(accepted)")
+        }, fallbackToSettings: true)
+
+        // Login your customer with externalId
+        // OneSignal.login("EXTERNAL_ID")
+                
         return true
     }
 }
