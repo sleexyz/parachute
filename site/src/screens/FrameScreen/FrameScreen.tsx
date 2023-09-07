7c49d32e0753d7d4e1ec94e1382be138d13726f6
Sean Lee <freshdried@gmail.com>
2023-09-06 23:35:16 -0400

    Update anima.

--- site/src/screens/FrameScreen/FrameScreen.tsx
+++ site/src/screens/FrameScreen/FrameScreen.tsx
@@ -4,8 +4,8 @@ import { Frame } from "../../components/Frame";
 export const FrameScreen = (): JSX.Element => {
   return (
     <div className="flex flex-col items-center justify-center relative">
-      <div className="flex flex-col items-center gap-[282px] pt-[122px] pb-[269px] px-[120px] relative self-stretch w-full flex-[0_0_auto] [background:linear-gradient(180deg,_rgb(255,_255,_255)_0%,_rgb(255,_255,_255)_0.01%,_rgb(255,_222,_187)_100%)]">
-        <div className="flex max-w-[1440px] items-start justify-between relative self-stretch w-full flex-[0_0_auto]">
+      <div className="flex flex-col items-center gap-[282px] pt-[122px] pb-[269px] px-0 relative self-stretch w-full flex-[0_0_auto] [background:linear-gradient(180deg,_rgb(255,_255,_255)_0%,_rgb(255,_255,_255)_0.01%,_rgb(255,_222,_187)_100%)]">
+        <div className="flex max-w-[1200px] items-start justify-between relative self-stretch w-full flex-[0_0_auto]">
           <div className="inline-flex flex-col items-start gap-[96px] pt-0 pb-[59px] px-0 relative self-stretch flex-[0_0_auto]">
             <div className="relative w-[445px] h-[86px]">
               <div className="absolute w-[337px] h-[86px] top-0 left-[110px]">
@@ -36,14 +36,14 @@ export const FrameScreen = (): JSX.Element => {
             src="/img/iphone-14-pro-copy-1-2560x1355-1-1.png"
           />
         </div>
-        <div className="flex flex-col items-start gap-[200px] relative self-stretch w-full flex-[0_0_auto] rounded-[5px] overflow-hidden">
+        <div className="flex flex-col max-w-[1200px] items-start gap-[161px] relative self-stretch w-full flex-[0_0_auto] rounded-[5px] overflow-hidden">
           <Frame className="!self-stretch !flex-[0_0_auto] !w-full" property1="default" />
           <Frame className="!self-stretch !flex-[0_0_auto] !w-full" property1="variant-3" />
           <Frame className="!self-stretch !flex-[0_0_auto] !w-full" property1="variant-2" />
         </div>
       </div>
       <div className="flex flex-col h-[1024px] items-center justify-center gap-[10px] relative self-stretch w-full bg-black">
-        <div className="flex flex-col max-w-[1440px] items-start justify-center gap-[103px] px-[120px] py-0 relative self-stretch w-full flex-[0_0_auto]">
+        <div className="flex flex-col max-w-[1200px] items-start justify-center gap-[103px] relative self-stretch w-full flex-[0_0_auto]">
           <img className="relative w-[149.39px] h-[149.35px]" alt="Group" src="/img/group-399.png" />
           <p className="relative w-[630px] h-[284px] [font-family:'Space_Grotesk',_Helvetica] font-normal text-white text-[48px] tracking-[0] leading-[normal]">
             <span className="[font-family:'Space_Grotesk',_Helvetica] font-normal text-white text-[48px] tracking-[0]">
