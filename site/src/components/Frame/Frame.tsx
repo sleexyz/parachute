432eb3426ee2ac01292e2c7276193ea6ff3ae6d0
Sean Lee <freshdried@gmail.com>
2023-09-06 23:35:16 -0400

    Update anima.

--- site/src/components/Frame/Frame.tsx
+++ site/src/components/Frame/Frame.tsx
@@ -38,110 +38,84 @@ export const Frame = ({ property1, className }: Props): JSX.Element => {
           {property1 === "variant-2" && <>DETOX</>}
         </div>
       </div>
-      <div className="[font-family:'Proxima_Soft-Regular',_Helvetica] mt-[-1.00px] tracking-[0] text-[32px] flex-1 text-transparent relative font-normal leading-[43px]">
+      <div className="[font-family:'Proxima_Soft-Regular',_Helvetica] mt-[-1.00px] tracking-[0] text-[32px] flex-1 text-transparent relative font-normal leading-[50px]">
         <span className={`${property1 === "variant-2" ? "text-[#ffffffcc]" : "text-[#00000099]"}`}>
-          {property1 === "variant-3" && <>By delaying reward, Parachute </>}
-
           {property1 === "variant-2" && <>We live in a world with </>}
 
           {property1 === "default" && <>Parachute works by </>}
+
+          {property1 === "variant-3" && <>By delaying reward, Parachute </>}
         </span>
         <span
           className={`[font-family:'Proxima_Soft-Bold',_Helvetica] font-bold ${
             property1 === "variant-2" ? "text-white" : "text-black"
           }`}
         >
-          {property1 === "default" && <>delaying</>}
+          {property1 === "default" && <>delaying social media feeds</>}
 
           {property1 === "variant-3" && <>deprograms</>}
 
           {property1 === "variant-2" && <>infinite</>}
         </span>
-        {["variant-2", "variant-3"].includes(property1) && (
-          <span className={`${property1 === "variant-2" ? "text-[#ffffffcc]" : "text-[#00000099]"}`}>
-            {property1 === "variant-3" && (
-              <>
-                {" "}
-                the circuits that keeps you checking your phone all the time.
-                <br />
-                <br />
-                <br />
-                Stop the{" "}
-              </>
-            )}
-
-            {property1 === "variant-2" && <> scroll yet we have </>}
-          </span>
-        )}
-
-        <span
-          className={`[font-family:'Proxima_Soft-Bold',_Helvetica] font-bold ${
-            property1 === "default" ? "text-[#00000099]" : property1 === "variant-3" ? "text-black" : "text-white"
-          }`}
-        >
-          {property1 === "variant-2" && <>finite</>}
-
-          {property1 === "default" && <>&nbsp;</>}
+        <span className={`${property1 === "variant-2" ? "text-[#ffffffcc]" : "text-[#00000099]"}`}>
+          {property1 === "default" && (
+            <>
+              {" "}
+              from loading, without removing the ability to message and post.
+              <br />
+              <br />
+              <br />
+              Stop getting{" "}
+            </>
+          )}
 
           {property1 === "variant-3" && (
             <>
-              compulsive checking
+              {" "}
+              the circuits that keeps you checking your phone all the time.
+              <br />
+              <br />
+              <br />
+              Stop the{" "}
+            </>
+          )}
+
+          {property1 === "variant-2" && <> scroll yet we have </>}
+        </span>
+        <span
+          className={`[font-family:'Proxima_Soft-Bold',_Helvetica] font-bold ${
+            property1 === "variant-2" ? "text-white" : "text-black"
+          }`}
+        >
+          {property1 === "default" && <>sucked in </>}
+
+          {property1 === "variant-3" && <>compulsive checking </>}
+
+          {property1 === "variant-2" && <>finite</>}
+        </span>
+        <span className={`${property1 === "variant-2" ? "text-[#ffffffcc]" : "text-[#00000099]"}`}>
+          {property1 === "default" && <>every time you check a message.</>}
+
+          {property1 === "variant-3" && <>from breaking your flow.</>}
+
+          {property1 === "variant-2" && (
+            <>
+              {" "}
+              energy and attention.
+              <br />
+              <br />
+              <br />
+              Parachute makes social media consumption sustainable by default.
               <br />
             </>
           )}
         </span>
-        {["variant-2", "variant-3"].includes(property1) && (
-          <span className={`${property1 === "variant-3" ? "text-[#00000099]" : "text-[#ffffffcc]"}`}>
-            {property1 === "variant-2" && (
-              <>
-                {" "}
-                energy and attention.
-                <br />
-                <br />
-                <br />
-                Parachute makes social media consumption sustainable by default.
-                <br />
-              </>
-            )}
-
-            {property1 === "variant-3" && <>from breaking your flow.</>}
-          </span>
-        )}
-
-        {["default", "variant-2"].includes(property1) && (
-          <span
-            className={`[font-family:'Proxima_Soft-Bold',_Helvetica] font-bold ${
-              property1 === "default" ? "text-black" : "text-white"
-            }`}
-          >
-            {property1 === "variant-2" && <>No more binging and purging apps.</>}
-
-            {property1 === "default" && <>social media feeds</>}
-          </span>
-        )}
-
-        {property1 === "default" && (
-          <>
-            <p>
-              <span className="text-[#00000099]">
-                {" "}
-                from loading, without removing the ability to message and post.
-                <br />
-                <br />
-                <br />
-                Stop getting{" "}
-              </span>
-            </p>
-            <p>
-              <span className="[font-family:'Proxima_Soft-Bold',_Helvetica] font-bold text-black">
-                sucked in
-                <br />
-              </span>
-            </p>
-            <p>
-              <span className="text-[#00000099]">every time you check a message.</span>
-            </p>
-          </>
+        {property1 === "variant-2" && (
+          <p>
+            <span className="[font-family:'Proxima_Soft-Bold',_Helvetica] font-bold text-white">
+              No more binging and purging apps.
+            </span>
+          </p>
         )}
       </div>
     </div>
